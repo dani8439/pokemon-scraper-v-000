56e6f7aec6bde756abba54e880181880c9b7df78
Dani Schuhman <dani8439@aol.com>
2018-08-01 16:27:06 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -24,7 +24,7 @@ class Pokemon
     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
   end
 
-  def self.alter_hp(hp: 59, db) 
+  def self.alter_hp(new_hp, db) 
   end
 
 end
